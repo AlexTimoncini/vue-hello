b60df6811d8046da2c1bd11dc89a138e5e18814d
AlexTimoncini <alextimonci@gmail.com>
2023-06-01 15:29:31 +0200

    vue ex

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      message: 'Ci ho messo una mezza ora ma ce lho fatta',
+      image: 'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80'
+    }
+  }
+}).mount('#app')
